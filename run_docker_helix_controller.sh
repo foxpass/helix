cc74af6a08bb961b1c77129cb65c768bc77d7afd
Karthik Gooli <karthik@foxpass.com>
2022-05-10 19:47:46 -0500

    Add command to add cluster to zk before the controller starts

--- run_docker_helix_controller.sh
+++ run_docker_helix_controller.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 ZKSRVR="${ZK:-host.docker.internal:2199}"
 CLUSTER="${CNAME:-MYCLUSTER}"
+
+./helix-core/target/helix-core-pkg/bin/helix-admin.sh --zkSvr $ZKSRVR --addCluster $CLUSTER
 ./helix-core/target/helix-core-pkg/bin/run-helix-controller.sh --zkSvr $ZKSRVR --cluster $CLUSTER 2>&1
